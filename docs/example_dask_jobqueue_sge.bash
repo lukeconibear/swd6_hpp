71b737d2bc3fabc6bfa31b800a251fda00affbba
Luke Conibear <laconibear@gmail.com>
2022-02-18 17:28:31 +0000

    Proof read parallelisation page

--- docs/example_dask_jobqueue_sge.bash
+++ docs/example_dask_jobqueue_sge.bash
@@ -1,8 +1,4 @@
 #!/bin/bash -l
-#$ -cwd -V
-#$ -l h_rt=01:00:00
-#$ -pe smp 1
-#$ -l h_vmem=1G
 
 # ensure linear algebra libraries using 1 thread
 # https://docs.dask.org/en/stable/array-best-practices.html#avoid-oversubscribing-threads
